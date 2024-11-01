dbef7e53c2a5b446f7bb012015ea300851acf1e1
Liceth Rozo <ja.parrar12@uniandes.edu.co>
2024-11-01 10:59:57 -0500

    Generacion de los ambientes y creacion de metodo consultar cafes

--- src/app/cafe/cafe.service.ts
+++ src/app/cafe/cafe.service.ts
@@ -1,10 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Cafe } from './cafe.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CafeService {
 
+  private apiUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
+
+  getCafes(): Observable<Cafe[]> {
+    return this.http.get<Cafe[]>(this.apiUrl);
+  }
 }
